b7a7f38207a7177790f2f6f6ce22d609e1f751c3
Anthony Nemitz <anemitz@gmail.com>
2012-07-24 20:53:12 -0700

    adding a way to get the authorization header

--- flask_common/utils.py
+++ flask_common/utils.py
@@ -1,4 +1,5 @@
 import csv
+import base64
 
 """
 Wrapper around csv reader that ignores non utf-8 chars and strips the record
@@ -27,3 +28,14 @@ def smart_unicode(s, encoding='utf-8', errors='strict'):
     elif not isinstance(s, unicode):
         s = s.decode(encoding, errors)
     return s
+
+def get_authorization_header(request):
+    if 'AUTHORIZATION' in request.headers:
+        authorization = request.headers['AUTHORIZATION'].split()
+        if len(authorization) == 2 and authorization[0].lower() == 'basic':
+            try:
+                authorization_parts = base64.b64decode(authorization[1]).partition(':')
+                return smart_unicode(authorization_parts[0]), smart_unicode(authorization_parts[2]), True
+            except (TypeError, UnicodeDecodeError):
+                pass
+    return None, None, False
