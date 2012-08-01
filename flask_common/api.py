696ed918748e645f8d5b3502eac0f3fc0b7a6558
Thomas Steinacher <tom@eggdrop.ch>
2012-07-31 18:01:04 -0700

    Support for async API using grequests.

--- flask_common/api.py
+++ flask_common/api.py
@@ -1,4 +1,5 @@
 import requests
+import grequests
 import base64
 import json
 import urllib
@@ -7,32 +8,51 @@ class APIError(Exception):
     pass
 
 class Client(object):
-    def __init__(self, base_url, api_key):
+    def __init__(self, base_url, api_key, async=False):
         assert base_url
         self.base_url = base_url
         self.api_key = api_key
+        self.async = async
+        if async:
+            import grequests
+            self.requests = grequests
+        else:
+            self.requests = requests.session()
 
-    def dispatch(self, method, endpoint, data=None):
+    def dispatch(self, method_name, endpoint, data=None):
+        method = getattr(self.requests, method_name)
+
         response = method(
             self.base_url+endpoint,
             data=data != None and json.dumps(data),
             headers={'Authorization' : 'Basic %s' % (base64.b64encode('%s:' % self.api_key), ), 'Content-Type': 'application/json'}
         )
 
-        if response.ok:
-            return json.loads(response.content)
+        if self.async:
+            return response
         else:
-            print response, response.content
-            raise APIError()
+            if response.ok:
+                return json.loads(response.content)
+            else:
+                print response, response.content
+                raise APIError()
 
     def get(self, endpoint, data=None):
         if data:
             endpoint += '/?'+urllib.urlencode(data)
-        return self.dispatch(requests.get, endpoint)
+        return self.dispatch('get', endpoint)
 
     def post(self, endpoint, data):
-        return self.dispatch(requests.post, endpoint+'/', data)
+        return self.dispatch('post', endpoint+'/', data)
 
     def put(self, endpoint, data):
-        return self.dispatch(requests.put, endpoint+'/', data)
+        return self.dispatch('put', endpoint+'/', data)
 
+    # Only for async requests
+    def map(self, requests):
+        if self.async:
+            import grequests
+            return [(
+                json.loads(response.content) if response.ok
+                else APIError()
+            ) for response in  grequests.map(requests)]
