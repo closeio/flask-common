f3271ab1278c2d5f2aec40517dcc1290c564e51d
Thomas Steinacher <tom@eggdrop.ch>
2012-08-30 18:11:49 -0700

    more helpful assert messages

--- flask_common/asserts.py
+++ flask_common/asserts.py
@@ -1,14 +1,12 @@
 def response_success(response, code=200):
     if 200 <= code < 300:
-        assert 200 <= response.status_code < 300
-    else:
-        assert code == response.status_code
+        assert 200 <= response.status_code < 300, 'Received %d response: %s' % (response.status_code, response.data)
+    assert code == response.status_code, 'Received %d response: %s' % (response.status_code, response.data)
 
 def response_error(response, code=400):
     if 400 <= code < 500:
-        assert 400 <= response.status_code < 500
-    else:
-        assert code == response.status_code
+        assert 400 <= response.status_code < 500, 'Received %d response: %s' % (response.status_code, response.data)
+    assert code == response.status_code, 'Received %d response: %s' % (response.status_code, response.data)
 
 def compare_req_resp(req_obj, resp_obj):
     for k,v in req_obj.iteritems():
