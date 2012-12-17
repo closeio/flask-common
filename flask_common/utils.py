87e622e362b7de0e223e8ccf11149ee24107057c
Anthony Nemitz <anemitz@gmail.com>
2012-12-17 14:08:11 -0800

    adding some unicode csv handling methods

--- flask_common/utils.py
+++ flask_common/utils.py
@@ -50,6 +50,18 @@ class DetailedSMTPHandler(SMTPHandler):
         except:
             self.handleError(record)
 
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
+
 """
 Wrapper around csv reader that ignores non utf-8 chars and strips the record
 """
