ec25d49bfa572cfc1cbd5a5187cdbffeb87a1cc1
Anthony Nemitz <anemitz@gmail.com>
2013-07-24 01:41:28 -0500

    CsvWriter improved to support more than string types during encoding

--- flask_common/utils.py
+++ flask_common/utils.py
@@ -118,7 +118,7 @@ class CsvWriter:
         self.encoder = codecs.getincrementalencoder(encoding)()
 
     def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
+        self.writer.writerow([s.encode("utf-8") if isinstance(s, basestring) else s for s in row])
         # Fetch UTF-8 output from the queue ...
         data = self.queue.getvalue()
         data = data.decode("utf-8")
