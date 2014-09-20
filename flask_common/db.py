712d7ec26df9db31894a2ab9e677afed62d13fb8
Thomas Steinacher <tom@eggdrop.ch>
2014-09-20 13:34:57 +0200

    Adding MongoReference, MongoEmbedded, MongoEmbeddedList

--- flask_common/db.py
+++ flask_common/db.py
@@ -1,8 +1,46 @@
+from bson import ObjectId
 import uuid
 import sqlalchemy as db
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, synonym
+
+__all__ = ['MongoReference', 'MongoEmbedded', 'MongoEmbeddedList', 'Base', 'UserBase']
+
+def MongoReference(field, ref_cls):
+    """
+    Reference to a MongoDB table. Note that a new instance is returned every
+    time we access, and no caching is performed.
+    """
+    # TODO: we should probably cache this
+    def _get(obj):
+        return ref_cls.objects.get(pk=getattr(obj, field))
+    def _set(obj, val):
+        if isinstance(val, ref_cls):
+            val = val.pk
+        if isinstance(val, ObjectId):
+            val = str(val)
+        setattr(obj, field, val)
+    return synonym(field, descriptor=property(_get, _set))
+
+def MongoEmbedded(field, emb_cls):
+    """
+    Converts the JSON value to/from an EmbeddedDocument. Note that a new
+    instance is returned every time we access and we must reassign any changes
+    back to the model.
+    """
+    def _get(obj):
+        return emb_cls._from_son(getattr(obj, field))
+    def _set(obj, val):
+        setattr(obj, field, val.to_mongo())
+    return synonym(field, descriptor=property(_get, _set))
+
+def MongoEmbeddedList(field, emb_cls):
+    def _get(obj):
+        return [emb_cls._from_son(item) for item in getattr(obj, field)]
+    def _set(obj, val):
+        setattr(obj, field, [item.to_mongo() for item in val])
+    return synonym(field, descriptor=property(_get, _set))
 
 # From https://code.launchpad.net/~stefanor/ibid/sqlalchemy-0.6-trunk/+merge/66033
 class PGSQLModeListener(object):
@@ -15,6 +53,10 @@ class Base(object):
     id = db.Column(UUID, default=lambda: str(uuid.uuid4()), primary_key=True)
     created_at = db.Column(db.DateTime(), default=db.func.now())
     updated_at = db.Column(db.DateTime(), default=db.func.now(), onupdate=db.func.now())
+
+    @property
+    def pk(self):
+        return self.id
 
     __mapper_args__ = {
         'order_by': db.desc('updated_at')
