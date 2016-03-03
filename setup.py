2301f760736f9c8e489bd365b6e5633735cb1a98
Lucas Vogelsang <l@lucasvo.com>
2016-03-02 18:06:34 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='flask-common',
+    version='0.1',
+    url='http://github.com/closeio/flask-common',
+    license='MIT',
+    description='Close.io internal flask helpers',
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ],
+    packages=[
+        'flask_common',
+    ],
+)
