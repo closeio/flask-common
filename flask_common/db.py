e0af68aad010e6238dfc8d6f5eeaf0323d387e6c
Stefan Wojcik <wojcikstefan@gmail.com>
2018-08-03 09:53:55 -0400

    use a queryset object instead

--- flask_common/db.py
+++ flask_common/db.py
@@ -7,24 +7,26 @@ from sqlalchemy.orm import relationship, synonym
 
 __all__ = ['MongoReference', 'MongoEmbedded', 'MongoEmbeddedList', 'Base', 'UserBase']
 
-def MongoReference(field, ref_cls, queryset_attr='objects'):
+def MongoReference(field, ref_cls, queryset=None):
     """
     Reference to a MongoDB document.
 
     The value is cached until an assignment is made.
 
-    If the Document class (`ref_cls`) implements a custom queryset manager,
-    you can switch to it (instead of the default `Document.objects`) using
-    the `queryset_attr`.
+    To use a custom queryset (instead of the default `ref_cls.objects`),
+    pass it as the `queryset` kwarg.
     """
+    if queryset is None:
+        queryset = ref_cls.objects
+
     def _get(obj):
         if not hasattr(obj, '_%s__cache' % field):
-            qs= getattr(ref_cls, queryset_attr)
             setattr(
                 obj, '_%s__cache' % field,
-                qs.get(pk=getattr(obj, field))
+                queryset.get(pk=getattr(obj, field))
             )
         return getattr(obj, '_%s__cache' % field)
+
     def _set(obj, val):
         if hasattr(obj, '_%s__cache' % field):
             delattr(obj, '_%s__cache' % field)
@@ -33,6 +35,7 @@ def MongoReference(field, ref_cls, queryset_attr='objects'):
         if isinstance(val, ObjectId):
             val = str(val)
         setattr(obj, field, val)
+
     return synonym(field, descriptor=property(_get, _set))
 
 def MongoEmbedded(field, emb_cls):
