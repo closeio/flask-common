350553c7eb27441ca93fd5ba2276c3f967b2eed4
Stefan Wojcik <wojcikstefan@gmail.com>
2019-10-14 11:32:53 +0200

    Make _generate_pk a class method

--- flask_common/mongo/documents.py
+++ flask_common/mongo/documents.py
@@ -37,8 +37,9 @@ class RandomPKDocument(Document):
     def get_pk_prefix(cls):
         return cls._get_collection_name()[:4]
 
-    def _generate_pk(self):
-        return u'%s_%s' % (self.get_pk_prefix(), zbase62.b2a(os.urandom(32)))
+    @classmethod
+    def _generate_pk(cls):
+        return u'%s_%s' % (cls.get_pk_prefix(), zbase62.b2a(os.urandom(32)))
 
     def save(self, *args, **kwargs):
         old_id = self.id
