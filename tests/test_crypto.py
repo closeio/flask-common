0ac40e859a1e5843387d5e18572470546e6fc340
João Sampaio <joao.sampaio@close.com>
2019-10-16 15:07:29 -0300

    Add test for unversioned data that starts with 0

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -45,3 +45,15 @@ def test_with_versioned_corrupted_data():
     encrypted_data = aes_encrypt(key, data)[3:]
     with pytest.raises(AuthenticationError):
         assert aes_decrypt(key, encrypted_data) == data
+
+
+def test_with_unversioned_data_that_starts_with_byte_0():
+    data = 'test'
+
+    # These were generated using the old encrypt functions.
+    # We make sure the new functions can still decypher the old
+    # encrypted data.
+    key = 'E2\x02\x18\xe5\xb9\xba5V.Z\x8d}\\\xa0\x1a\x1b=\x89]}\xe4\xcf\x92_\xe2\x83\xeaI\xcb\xdd\xabo\xbc\xad\r\xef\xa2\xa2\xfa\xd0\x03\x98$\x8aM\xe5\x88l\x80\xadl\xef\x08t\xc8\xfd\x9b\t\x98\x7f\xcb\x10\x85'
+    encrypted_data = '\x00\x04\xa0\x01H\x00\x85eO{\x00\xe1\x05\xb1\xf4\xbc\x849\x16\x94\x96%\x9d<y\x07I\x05\xdf\xd1\x07\x94-t\x806-\x06#\x00\x93\xfew\x12\x84\xf4\xc1\xe7\x18\x81\xd2\xb1\xe0_\xed/\x99\xa8\x1d;Z\x93\x18h t\xf4\x91'
+
+    assert aes_decrypt(key, encrypted_data) == data
