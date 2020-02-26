be927bb0b28677828a18be7a49aba903899f60f1
João Sampaio <joao.sampaio@close.com>
2020-02-26 15:45:36 -0300

    Drop support for v0 encryption (#77)

--- flask_common/crypto.py
+++ flask_common/crypto.py
@@ -1,6 +1,6 @@
 """This file supports versioned encrypted information.
 
-* Version 0: marked with first byte `\x00`
+* Version 0: marked with first byte `\x00` (deprecated)
 
 Implemented with `pycrypto`.
 
@@ -8,7 +8,7 @@ In this version, data is returned from `aes_encrypt` in the format:
 
 [VERSION 0 byte][IV 32 bytes][Encrypted data][HMAC 32 bytes]
 
-This format comes from a erroneous implementation that used an IV of
+This format comes with an erroneous implementation that used an IV of
 32 bytes when AES expects IVs of 16 bytes, and the library used at the
 time (`pycrypto`) silently truncated the IV for us.
 
@@ -23,7 +23,7 @@ In this version, data is returned from `aes_encrypt` in the format:
 This version came into existence to fix the wrong-sized IVs from
 version 0.
 
-Current code decrypts both versions, encrypts to version 1.
+Current code decrypts from and encrypts to version 1 only.
 
 In CTR mode, IV is also often called a Nonce (in `cryptography`'s
 public interface, for example).
@@ -48,13 +48,11 @@ AES_KEY_SIZE = 32  # 256 bits
 HMAC_KEY_SIZE = 32  # 256 bits
 KEY_LENGTH = AES_KEY_SIZE + HMAC_KEY_SIZE
 
-V0_IV_SIZE = 32  # 256 bits, wrong size used in version 0
 IV_SIZE = 16  # 128 bits
 
 HMAC_DIGEST = hashlib.sha256
 HMAC_DIGEST_SIZE = hashlib.sha256().digest_size
 
-V0_MARKER = b'\x00'
 V1_MARKER = b'\x01'
 
 
@@ -68,8 +66,7 @@ class AuthenticationError(Exception):
 
 """
 Helper AES encryption/decryption methods. Uses AES-CTR + HMAC for authenticated
-encryption. The same key/iv combination must never be reused to encrypt
-different messages.
+encryption. The same key/iv combination must never be reused.
 """
 
 
@@ -94,12 +91,7 @@ def aes_decrypt(key, data):
     extracted_version = data[0:1]
     data = data[1:]
 
-    # In version 0, we used IVs with wrong sizes. We need to take this
-    # into account when separating encrypted data from their IVs.
-    if extracted_version == V0_MARKER:
-        iv = data[:V0_IV_SIZE]
-        data = data[V0_IV_SIZE:]
-    elif extracted_version == V1_MARKER:
+    if extracted_version == V1_MARKER:
         iv = data[:IV_SIZE]
         data = data[IV_SIZE:]
     else:
@@ -107,7 +99,7 @@ def aes_decrypt(key, data):
             'Found invalid version marker: {!r}'.format(extracted_version)
         )
 
-    return aes_decrypt_iv(key, data, iv, extracted_version)
+    return aes_decrypt_iv(key, data, iv)
 
 
 # Encrypt + sign using provided IV.
@@ -125,22 +117,13 @@ def aes_encrypt_iv(key, data, iv):
 
 # Verify + decrypt using provided IV.
 # Note: You should normally use aes_decrypt().
-def aes_decrypt_iv(key, data, iv, extracted_version):
+def aes_decrypt_iv(key, data, iv):
     aes_key = key[:AES_KEY_SIZE]
     hmac_key = key[AES_KEY_SIZE:]
     cipher = data[:-HMAC_DIGEST_SIZE]
     sig = data[-HMAC_DIGEST_SIZE:]
     if hmac.new(hmac_key, iv + cipher, HMAC_DIGEST).digest() != sig:
         raise AuthenticationError('message authentication failed')
-
-    # In version 0, we used IVs with wrong sizes. `pycrypto` was
-    # silently truncating those IVs for us before using them for
-    # encryption. We need to do the same thing here, since
-    # `cryptography` just expects the correctly-sized IV. Use the
-    # **last** `IV_SIZE` bytes of the IV.
-    if extracted_version == V0_MARKER:
-        iv = iv[IV_SIZE:]
-
     decryptor = Cipher(
         algorithms.AES(aes_key), modes.CTR(iv), backend=backend
     ).decryptor()
