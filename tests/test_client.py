48e64718547382603ccadd8f8857e2a06141f755
Vyacheslav Tverskoy <tsx@tsx.su>
2020-02-26 17:05:31 +0500

    Test for flask 1 client

--- tests/test_client.py
+++ tests/test_client.py
@@ -16,3 +16,14 @@ def test_api_client_basic_auth():
 
     headers = client.get_headers(client.api_key)
     assert headers == Headers([('Authorization', 'Basic MTIzNDU2Og==',)])
+
+
+def test_json():
+    app = Flask('test')
+
+    @app.route('/', methods=['GET'])
+    def view():
+        return {'ok': 1}
+
+    client = ApiClient(app)
+    assert client.get('/').json['ok'] == 1
